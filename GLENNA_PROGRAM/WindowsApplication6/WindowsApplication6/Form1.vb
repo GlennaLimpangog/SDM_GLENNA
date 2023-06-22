61bdf00e46ee7b82aeff91d29596fec554cd3532
GlennaLimpangog <135185433+GlennaLimpangog@users.noreply.github.com>
2023-06-22 16:15:06 +0800

    Add files via upload

---
+++ GLENNA_PROGRAM/WindowsApplication6/WindowsApplication6/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.AppStat = " Full Version " Then
+            Me.Text = My.Settings.AppStat
+        Else
+
+
+            If My.Settings.LoadCount < 0 Then
+                Form2.ShowDialog()
+            Else
+                Me.Text = My.Settings.AppStat & " :: Count > " &
+                    My.Settings.LoadCount
+                My.Settings.LoadCount -= 1
+                My.Settings.Save()
+
+            End If
+        End If
+    End Sub
+End Class
